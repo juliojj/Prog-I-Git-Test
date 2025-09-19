0ac7666ec5dc7b2bee1f6440dcb4cf9d676d2d69
Julio Silveira <julio.silveira@ub.edu>
2025-09-19 12:15:41 +0000

    Afegint la data al missatge

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld { 
     public static void main(String[] args) { 
-        System.out.println("Hello, World of Git!"); 
+        System.out.println("Hello, World of Git 2025!"); 
     } 
 }
